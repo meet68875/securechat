523ec0a4a527961e29704f5921b6f2ec75a6ee64
my-first-git-project <meet68875@gmail.com>
2025-12-08 23:09:59 +0530

    chnage into layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,6 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import ClientProvider from './ClientProvider';
-import AuthGuard from '../../components/AuthGuard';
 
 export const metadata = {
   title: 'SecureChat',
@@ -15,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
       <body className="h-full bg-gray-50">
-        <ClientProvider><AuthGuard>{children}</AuthGuard></ClientProvider>
+        <ClientProvider>{children}</ClientProvider>
       </body>
     </html>
   );
